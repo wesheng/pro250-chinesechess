cd1642f309557fb5aa3f4b2fd60997db36dac647
WS <>
2018-08-01 11:11:22 -0600

    Add unit testing

---
+++ UnitTests/PieceTests.cs
@@ -0,0 +1,18 @@
+﻿using ChineseChess;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class PieceTests
+    {
+        [Test]
+        [TestCase(Piece.RedKnight, Piece.BlackKnight)]
+        public void CanInvertPiece(Piece piece, Piece expect)
+        {
+            Piece inverted = piece.Invert();
+
+            Assert.AreEqual(expect, inverted, "Piece should be inverted");
+        }
+    }
+}
